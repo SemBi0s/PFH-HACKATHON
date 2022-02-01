3d31a335f1fb40aeb57c0ce0e57ce6fbd1285dc6
Alexandre Daumard <42819456+SemBi0s@users.noreply.github.com>
2022-02-01 15:41:47 +0100

    Update settings.py

--- HACKATHONAPI/settings.py
+++ HACKATHONAPI/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e(=95af)qh$yva9ui@))u3a&4m+9iv9w89bk0ya&v^m=89uim0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
